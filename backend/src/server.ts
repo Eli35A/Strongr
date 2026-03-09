c9a80d785fe309fa8a736bbab1474371dd6bb0c5
Eli35 <eli357a@gmail.com>
2026-03-09 15:45:53 +0200

    added unit tests

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,38 +1,10 @@
-import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import dotenv from 'dotenv';
+import app from './app';
 import connectDB from './config/db';
-import path from 'path';
-import authRoutes from './routes/authRoutes';
-import userRoutes from './routes/userRoutes';
-import postRoutes from './routes/postRoutes';
 
-dotenv.config();
-
-connectDB();
-
-const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true
-}));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-
-app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
-
-app.use('/auth', authRoutes);
-app.use('/users', userRoutes);
-app.use('/posts', postRoutes);
-
-app.get('/', (req, res) => {
-    res.send('Strongr API is running...');
-});
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
 });
